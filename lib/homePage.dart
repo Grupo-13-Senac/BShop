e56d269791221c2d4cd2b62ce19b621e4606d091
Lucas Bustamante <lucascostabustamante@gmail.com>
2023-12-13 00:04:30 -0300

    update

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -1,4 +1,3 @@
-import 'package:animated_bottom_navigation_bar/animated_bottom_navigation_bar.dart';
 import 'package:ff_navigation_bar_plus/ff_navigation_bar_plus.dart';
 import 'package:flutter/material.dart';
 
